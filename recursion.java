ca89ba79dd2832aefec478cb716c0e0f60ea42d2
ph3b3 <phoebeleong8@gmail.com>
2021-08-02 14:30:29 +1000

    four lines!

--- recursion.java
+++ recursion.java
@@ -1,11 +1,4 @@
 class recursion {
-    static void recursion() {
-    System.out.println("This is recursion.\n");
-
-    recursion();
-    }
-
-    public static void main(String[] args) {
-        recursion();
-    }
+    static void recursion() { System.out.println("This is recursion.\n"); }
+    public static void main(String[] args) { recursion(); }
 }
