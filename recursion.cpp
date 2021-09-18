a7985a95b06ee75750eacf17a963f9cb56248182
ph3b3 <phoebeleong8@gmail.com>
2021-09-19 09:47:44 +1000

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -1,3 +1,2 @@
 #include <iostream>
-void recursion() { std::cout << "This is recursion." << std::endl; }
-int main() { while (true) recursion(); }
+int main() { while (true) std::cout << "This is layman recursion.\n"; }
